8f64bd15ab5ba6ebc26bb97dcf5ce0fc19f94017
anheejeong <eyrt6973@naver.com>
2023-07-27 00:49:48 +0900

    useEffect + cleanupCode

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,12 +11,26 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid( // 이 코드가 타이핑 할때마다 수십번 발생하는 것을 피하기 위함
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500); // 타이핑 후 500밀리초 지나면 실행
+
+    return () => {  // 클린업 코드가 실행되어야 useEffect 실행
+      console.log('CLEANUP');
+      clearTimeout(identifier); // 클린업 함수가 실행될 때마다 클린업 함수가 실행되기 전에 설정된 타이머를 지움
+    };
+
+  }, [setFormIsValid, enteredEmail, enteredPassword]); //setFormIsValid는 state에 의해 관리되고 있으므로 생략 가능
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    // setFormIsValid(
+    //   enteredEmail.includes('@') && enteredPassword.trim().length > 6
+    // );
   };
 
   const passwordChangeHandler = (event) => {
@@ -44,9 +58,8 @@ const Login = (props) => {
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
+          className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+            }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
@@ -58,9 +71,8 @@ const Login = (props) => {
           />
         </div>
         <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
+          className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''
+            }`}
         >
           <label htmlFor="password">Password</label>
           <input
